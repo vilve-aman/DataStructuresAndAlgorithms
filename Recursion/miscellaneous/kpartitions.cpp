e5cd20f990a0ac8d787b994ef45ff9be339a5e38
Aman@Legion5i <vilve.aman@gmail.com>
2021-12-25 17:37:23 +0530

    problem statement links modified

--- Recursion/miscellaneous/kpartitions.cpp
+++ Recursion/miscellaneous/kpartitions.cpp
@@ -1,5 +1,5 @@
 // problem statement 
-// https://www.geeksforgeeks.org/count-number-of-ways-to-partition-a-set-into-k-subsets/
+//  https://www.geeksforgeeks.org/count-number-of-ways-to-partition-a-set-into-k-subsets/
 // https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/k-partitions-official/ojquestion
 
 #include <bits/stdc++.h>
